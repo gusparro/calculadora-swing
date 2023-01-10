0e16befbba5aed8bfcf399d90ab88588fc932a64
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-01-10 18:37:41 -0300

    Criando primeiro frame.

--- src/com/gusparro/calculadora/swing/Calculator.java
+++ src/com/gusparro/calculadora/swing/Calculator.java
@@ -1,13 +1,16 @@
 package com.gusparro.calculadora.swing;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Calculator extends JFrame {
     public Calculator() {
+        setSize(232, 322);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        new Calculator();
     }
 }
