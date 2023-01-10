20def017fa0d6d2f0cedfb3b99c3f84b3d582366
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-01-10 18:32:55 -0300

    Criando o projeto.

---
+++ src/com/gusparro/calculadora/swing/Calculator.java
@@ -0,0 +1,13 @@
+package com.gusparro.calculadora.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculator extends JFrame {
+    public Calculator() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
