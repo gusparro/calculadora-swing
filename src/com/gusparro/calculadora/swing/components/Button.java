40779815523ca5b7eeed250749d52801a6d477aa
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-01-11 10:13:33 -0300

    Customizando o Button.

--- src/com/gusparro/calculadora/swing/components/Button.java
+++ src/com/gusparro/calculadora/swing/components/Button.java
@@ -1,6 +1,20 @@
 package com.gusparro.calculadora.swing.components;
 
 import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Color.BLACK;
+import static java.awt.Color.WHITE;
+import static java.awt.Font.PLAIN;
 
 public class Button extends JButton {
+
+    public Button(String text, Color color) {
+        setText(text);
+        setOpaque(true);
+        setBackground(color);
+        setFont(new Font("courier", PLAIN, 25));
+        setForeground(WHITE);
+        setBorder(BorderFactory.createLineBorder(BLACK));
+    }
 }
