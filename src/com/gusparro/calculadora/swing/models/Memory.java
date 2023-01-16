f1a31c758ce4e42cbecf119f4eaaa8c923705eea
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-01-16 10:32:38 -0300

    Criando método que detecta o tipo de evento disparado.

--- src/com/gusparro/calculadora/swing/models/Memory.java
+++ src/com/gusparro/calculadora/swing/models/Memory.java
@@ -3,6 +3,8 @@ package com.gusparro.calculadora.swing.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.gusparro.calculadora.swing.models.TypeOfEvent.*;
+
 public class Memory {
 
     private static final Memory INSTANCE = new Memory();
@@ -26,12 +28,39 @@ public class Memory {
     }
 
     public void processInput(String value) {
+        TypeOfEvent typeOfEvent = detectTyoeOfEvent(value);
+        System.out.println(typeOfEvent);
+
         currentText += value;
 
         listeners.forEach(listener -> listener.updatValue(getCurrentText()));
     }
 
-    public void validateInput(String value) {
+    public TypeOfEvent detectTyoeOfEvent(String value) {
+        if (currentText.isBlank() && value.equals("0"))
+            return null;
 
+        try {
+            Integer.parseInt(value);
+            return NUMBER;
+        } catch (NumberFormatException exception) {
+            if ("AC".equals(value)) {
+                return CLEAN;
+            } else if (",".equals(value)) {
+                return COMMA;
+            } else if ("*".equals(value)) {
+                return MULTI;
+            } else if ("/".equals(value)) {
+                return DIV;
+            } else if ("+".equals(value)) {
+                return SUM;
+            } else if ("-".equals(value)) {
+                return SUB;
+            } else if ("=".equals(value)) {
+                return EQUAL;
+            }
+        }
+
+        return null;
     }
 }
---
+++ src/com/gusparro/calculadora/swing/models/TypeOfEvent.java
@@ -0,0 +1,7 @@
+package com.gusparro.calculadora.swing.models;
+
+public enum TypeOfEvent {
+
+    CLEAN, NUMBER, COMMA, MULTI, DIV, SUM, SUB, EQUAL;
+
+}
